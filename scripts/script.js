ec0f49bd70f6997a881be63a291f04ec6be42cb0
Daniil Bushkovskii <Buschkovsky@gmail.com>
2022-05-23 23:40:37 +0700

    refactor: редактирование комментариев и элементов

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,12 @@
+//Переменные для открытия/закрытия попапа
 const editButton = document.querySelector('.profile__button_type_edit');
 const popupOpen = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__close-button');
-//форма
-let form = document.querySelector('.form');
+// Данные профиля
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
+//форма
+let form = document.querySelector('.form');
 let formName = document.querySelector('.form__item_el_name');
 let formAbout = document.querySelector('.form__item_el_about');
 let buttonSave = document.querySelector('.form__button');
