7d1ee10b3b04c23b78071585c23c7a7502881583
Daniil Bushkovskii <Buschkovsky@gmail.com>
2022-06-06 11:20:18 +0700

    feat: Added the ability to delete a card

--- scripts/index.js
+++ scripts/index.js
@@ -45,6 +45,8 @@ let newItem = document.querySelector('#NewItem');
 let newItemTitle = document.querySelector('#NewItemTitle');
 let newItemLink = document.querySelector('#NewItemLink');
 
+
+
 function popupOpen(popup) {
    popup.classList.add('popup_opened');
 }
@@ -82,25 +84,56 @@ newItemCloseButton.addEventListener('click', () => {
    popupClose(popupNewItem);
 });
 
+
 //Функция добавление карточки
 function addCard(element) {
    const galleryTemplate = document.querySelector('#gallery-template').content;//Template
    const galleryItem = galleryTemplate.cloneNode(true);//Клонирование template
+   const trashButton = galleryItem.querySelector('.gallery__trash');
 
    galleryItem.querySelector('.gallery__title').textContent = element.name;
    galleryItem.querySelector('.gallery__image').src = element.link;
+   //Добавление id для карточки и кнопки удаления. Методы split и join добавлены потому что
+   //без них не удается найти нужные элементы по id.
+   galleryItem.querySelector('.gallery__item').id = element.name.split(' ').join('');
+   trashButton.id = element.name.split(' ').join('');
 
    //Лайк
-   galleryItem.querySelector('.gallery__like-button').addEventListener('click', function (event){
+   galleryItem.querySelector('.gallery__like-button').addEventListener('click', event =>{
       event.target.classList.toggle('gallery__like-button_active');
+   })
+
+   //Удаление карточки. Данный обработчик добавлен в функцию addCard(). Если обработчик вынести
+   // из addCard (пример в закомментированном коде, начиная со строки 151), тогда событие срабатывает только
+   // на первом элементе.
+   trashButton.addEventListener('click', event => {
+      // const eventTargetId = event.target.id;
+      // console.log(event.target.id);
+      // console.log(`ID:${event.target.id}`);
+      document.querySelector(`#${event.target.id}`).remove();
    })
 
    gallery.prepend(galleryItem);//Добавление карточки в список
 }
+
+
 //Добавление карточек из массива
    initialCards.forEach(element => {
       addCard(element);
    });
+
+//Добавление карточки, которую запросил пользователь
+newItem.addEventListener('submit', (evt) => {
+   evt.preventDefault();
+   let titleValue = newItemTitle.value;
+   let linkValue = newItemLink.value;
+   const card = {
+      name: titleValue,
+      link: linkValue
+   }
+   addCard(card);
+   popupClose(popupNewItem);
+});
 
 //Обработчик(объявленная функция)
 // function addCardHandler(evt) {
@@ -115,15 +148,15 @@ function addCard(element) {
 //    popupClose(popupNewItem);
 // }
 
-//Добавление карточки, которую запросил пользователь
-newItem.addEventListener('submit', (evt) => {
-   evt.preventDefault();
-   let titleValue = newItemTitle.value;
-   let linkValue = newItemLink.value;
-   const card = {
-      name: titleValue,
-      link: linkValue
-   }
-   addCard(card);
-   popupClose(popupNewItem);
-});
+
+//Удаление карточки
+// const trashButton = document.querySelector('.gallery__trash');
+// function removeCard(event) {
+//    const eventTargetId = event.target.id.split(' ').join('');
+//    console.log(event.target.id);
+//    console.log(`ID:${eventTargetId}`);
+//    document.querySelector(`#${eventTargetId}`).remove();
+// }
+//
+//
+// trashButton.addEventListener('click', removeCard);
