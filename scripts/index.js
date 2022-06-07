79f05618ef9e74910c68ace2c86b7d2247e655b7
Daniil Bushkovskii <Buschkovsky@gmail.com>
2022-06-07 17:21:46 +0700

    fix: fixed js code

--- scripts/index.js
+++ scripts/index.js
@@ -1,182 +1,111 @@
 const editButton = document.querySelector('.profile__button_type_edit');
-const popup = document.querySelector('.popup');
+const popupEditProfile = document.querySelector('#popupEdit');
 const profileCloseButton = document.querySelector('#profileCloseButton');
-const gallery = document.querySelector('.gallery__grid-container');//Контейнер списка
+const gallery = document.querySelector('.gallery__grid-container');
 const addButton = document.querySelector('.profile__button_type_add');
 const popupNewItem = document.querySelector('#popupAdd');
 const newItemCloseButton = document.querySelector('#NewItemCloseButton')
 const popupImageCloseButton = document.querySelector('#popupImageCloseButton');
-const form = document.querySelector('.popup__form');
-const newItem = document.querySelector('#NewItem');
-const newItemTitle = document.querySelector('#NewItemTitle');
-const newItemLink = document.querySelector('#NewItemLink');
-let profileName = document.querySelector('.profile__name');
-let profileAbout = document.querySelector('.profile__about');
-let profileFormName = document.querySelector('#ProfileName');
-let profileFormAbout = document.querySelector('#ProfileAbout');
-
-const initialCards = [
-   {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-   },
-   {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-   },
-   {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-   },
-   {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-   },
-   {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-   },
-   {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-   }
-];
+const formEditProfile = document.querySelector('#formProfile');
+const formAddCard = document.querySelector('#formAddCard');
+const addCardTitle = document.querySelector('#NewItemTitle');
+const addCardLink = document.querySelector('#NewItemLink');
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
+const profileFormName = document.querySelector('#ProfileName');
+const profileFormAbout = document.querySelector('#ProfileAbout');
+const galleryTemplate = document.querySelector('#gallery-template').content;
+const popupImage = document.querySelector('#popupImage');
+let galleryCard;
 
 
-function popupOpen(popup) {
+function openPopup(popup) {
    popup.classList.add('popup_opened');
 }
 
-function popupClose(popup) {
+function closePopup(popup) {
    popup.classList.remove('popup_opened');
 }
 
-function formSubmitHandler(evt) {
+function submitEditProfileForm(evt) {
    evt.preventDefault();
-   let profileNameValue = profileFormName.value;
-   let profileAboutValue = profileFormAbout.value;
+   profileName.textContent = profileFormName.value;
+   profileAbout.textContent = profileFormAbout.value;
+   closePopup(popupEditProfile);
+}
 
+function createCard (el) {
+   const galleryItem = galleryTemplate.cloneNode(true);
+   galleryItem.querySelector('.gallery__title').textContent = el.name;
+   galleryItem.querySelector('.gallery__image').src = el.link;
+   galleryItem.querySelector('.gallery__image').alt = el.name;
+   galleryItem.querySelector('.gallery__item').id = el.name.split(' ').join('');
+   galleryItem.querySelector('.gallery__trash').id = el.name.split(' ').join('');
 
-   profileName.textContent = profileNameValue;
-   profileAbout.textContent = profileAboutValue;
-   popupClose(popup);
+   //События
+   galleryItem.querySelector('.gallery__like-button').addEventListener('click', evt =>{
+      evt.target.classList.toggle('gallery__like-button_active');
+   })
+
+   // Удаление карточки.
+   galleryItem.querySelector('.gallery__trash').addEventListener('click', evt => {
+      evt.target.closest('.gallery__item').remove();
+   })
+
+   //Попап с изображением
+   galleryItem.querySelector('.gallery__image').addEventListener('click', evt => {
+      openPopup(popupImage);
+      popupImage.querySelector('.popup__image').src = evt.target.src;
+      popupImage.querySelector('.popup__image').alt = evt.target.alt;
+      popupImage.querySelector('#popupImageCaption').textContent = evt.target.alt;
+   })
+
+    return galleryCard = galleryItem;
 }
 
 //Функция добавление карточки
-function addCard(element) {
-   const galleryTemplate = document.querySelector('#gallery-template').content;//Template
-   const galleryItem = galleryTemplate.cloneNode(true);//Клонирование template
-   const trashButton = galleryItem.querySelector('.gallery__trash');
-   const galleryImage = galleryItem.querySelector('.gallery__image');
-
-   galleryItem.querySelector('.gallery__title').textContent = element.name;
-   galleryItem.querySelector('.gallery__image').src = element.link;
-
-   //Добавление id для карточки и кнопки удаления. Методы split и join добавлены потому что
-   //без них не удается найти нужные элементы по id.
-   galleryItem.querySelector('.gallery__item').id = element.name.split(' ').join('');
-   trashButton.id = element.name.split(' ').join('');
-
-   //Лайк
-   galleryItem.querySelector('.gallery__like-button').addEventListener('click', event =>{
-      event.target.classList.toggle('gallery__like-button_active');
-   })
-
-   //Удаление карточки. Данный обработчик добавлен в функцию addCard(). Если обработчик вынести
-   // из addCard (пример в закомментированном коде, начиная со строки 161), тогда событие срабатывает только
-   // на первом элементе на странице.
-   trashButton.addEventListener('click', event => {
-      // const eventTargetId = event.target.id;
-      // console.log(event.target.id);
-      // console.log(`ID:${event.target.id}`);
-      document.querySelector(`#${event.target.id}`).remove();
-   })
-
-   galleryImage.addEventListener('click', event => {
-      event.target;
-      popupOpen(popupImage);
-      popupImage.querySelector('.popup__image').src = event.target.src;
-
-   })
-
-   gallery.prepend(galleryItem);//Добавление карточки в список
+function addCard() {
+   gallery.prepend(galleryCard);
 }
 
-form.addEventListener('submit', formSubmitHandler);
+//Добавление карточек из массива
+initialCards.forEach(el => {
+   createCard(el);
+   addCard()
+});
 
 editButton.addEventListener('click', () => {
-   popupOpen(popup);
+   openPopup(popupEditProfile);
    profileFormName.value = profileName.textContent;
    profileFormAbout.value = profileAbout.textContent;
 });
 
+formEditProfile.addEventListener('submit', submitEditProfileForm);
+
 profileCloseButton.addEventListener('click', () => {
-   popupClose(popup);
+   closePopup(popupEditProfile);
 });
 
 addButton.addEventListener('click', () => {
-   popupOpen(popupNewItem);
+   openPopup(popupNewItem);
 });
 
 newItemCloseButton.addEventListener('click', () => {
-   popupClose(popupNewItem);
+   closePopup(popupNewItem);
 });
 
 popupImageCloseButton.addEventListener('click', () => {
-   popupClose(popupImage);
+   closePopup(popupImage);
 })
 
-//Добавление карточек из массива
-initialCards.forEach(element => {
-   addCard(element);
-});
-
 //Добавление карточки, которую запросил пользователь
-newItem.addEventListener('submit', (evt) => {
+formAddCard.addEventListener('submit', (evt) => {
    evt.preventDefault();
-   let titleValue = newItemTitle.value;
-   let linkValue = newItemLink.value;
    const card = {
-      name: titleValue,
-      link: linkValue
+      name: addCardTitle.value,
+      link: addCardLink.value
    }
-   addCard(card);
-   popupClose(popupNewItem);
+   createCard(card)
+   addCard();
+   closePopup(popupNewItem);
 });
-
-//Обработчик(объявленная функция)
-// function addCardHandler(evt) {
-//    evt.preventDefault();
-//    let titleValue = newItemTitle.value;
-//    let linkValue = newItemLink.value;
-//    const card = {
-//       name: titleValue,
-//       link: linkValue
-//    }
-//    addCard(card);
-//    popupClose(popupNewItem);
-// }
-
-
-//Удаление карточки
-// const trashButton = document.querySelector('.gallery__trash');
-// function removeCard(event) {
-//    const eventTargetId = event.target.id.split(' ').join('');
-//    console.log(event.target.id);
-//    console.log(`ID:${eventTargetId}`);
-//    document.querySelector(`#${eventTargetId}`).remove();
-// }
-//
-//
-// trashButton.addEventListener('click', removeCard);
-
-//Попап с картинкой
-// const galleryImage = document.querySelector('.gallery__image');
-// const popupImage = document.querySelector('#popupImage');
-//
-// galleryImage.addEventListener('click', event => {
-//    event.target;
-//    popupOpen(popupImage);
-//    const Image = popupImage.querySelector('.popup__image');
-//    Image.src = event.target.src;
-// })
